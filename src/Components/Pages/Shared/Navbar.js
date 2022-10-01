05ba64289434b10ac53c03ba055066b20be072f8
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-10-01 15:01:16 +0600

    add

--- src/Components/Pages/Shared/Navbar.js
+++ src/Components/Pages/Shared/Navbar.js
@@ -7,14 +7,11 @@ import search from '../../Assests/Vector.png'
 const Navbar = () => {
     return (
         <div>
-            <div class="navbar h-20 bg-[#FFFFFF] shadow-lg">
+        <div class="navbar h-20 bg-[#FFFFFF] shadow-lg">
         <div class="navbar-start">
           <div class="dropdown">
             <img src={logo} className="my-4 ml-12" alt="" />
-            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li><a>Item 3</a></li>
-            </ul>
+            
           </div>
       
         </div>
@@ -28,12 +25,12 @@ const Navbar = () => {
           </ul>
         </div>
         <div class="navbar-end">
-             <div className="flex items-center ">
+             <div className="flex items-center hidden md:flex ">
                 <div>
                  <input type="text" placeholder="Search here" className="border rounded-l-lg bg-[#F0F0F0] bottom-2 h-11 border-none p-2 focus:outline-none" />
                 </div>
                 <div>
-                <img src={search} alt="" className="bg-[#F0F0F0] w-7 py-3 px-1 rounded-r-lg"   />
+                 <img src={search} alt="" className="bg-[#F0F0F0] w-7 py-3 px-1 rounded-r-lg"   />
                 </div>
              </div>
              <div className="flex">
@@ -42,6 +39,7 @@ const Navbar = () => {
                 <img src={face} alt="" className="mx-4" />
              </div>
         </div>
+        
         </div>
         </div>
     );
